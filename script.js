ad5b4df7e06845da4a3cea31e7eca298175894b6
Junus Ergin <JunusErgin@users.noreply.github.com>
2024-09-17 15:19:49 +0200

    Smooth character movement

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
 let frame = 0;
 let state = 'IDLE';
 let left = 300;
+let leftArrow = false;
+let rightArrow = false;
+
 
 setInterval(moveCharacter,75);
-setInterval(updateBackground, 1000 / 60);
+setInterval(updateGame, 1000 / 60);
 document.onkeydown = checkKey;
+document.onkeyup = unCheckKey;
 
 function checkKey(e) {
-
     e = e || window.event;
 
     if (e.keyCode == '37') {
        // left arrow
-       left -= 5;
+       leftArrow = true;
        setState('WALK');
     }
     else if (e.keyCode == '39') {
        // right arrow
-       left += 5;
+       rightArrow = true;
        setState('WALK');
     }
 }
 
-function updateBackground() {
+function unCheckKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+        leftArrow = false;
+    } else if (e.keyCode == '39') {
+        rightArrow = false;
+    }
+}
+
+function updateGame() {
     currentBackground.style.objectPosition = `${-left}px`;
+    if(leftArrow) {
+        left -= 5;
+    }
+    if(rightArrow) {
+        left += 5;
+    }
+
+    if(leftArrow || rightArrow) {
+        setState('WALK');
+    } else {
+        setState('IDLE');
+    }
 }
 
 function moveCharacter(){
     pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
     frame++;
+    if(leftArrow) {
+        pirate.style.transform = "scaleX(-1)";
+    }
+
+    if(rightArrow) {
+        pirate.style.transform = "scaleX(1)";
+    }
 
     if(frame == 7) {
         frame = 0;
