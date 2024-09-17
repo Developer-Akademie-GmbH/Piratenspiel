cd22cebdb719d14ecb13e0fc3ddbd14cb025a5fe
Junus Ergin <JunusErgin@users.noreply.github.com>
2024-09-17 14:40:26 +0200

    Move character

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 let frame = 0;
 let state = 'IDLE';
+let left = 300;
 
 setInterval(moveCharacter,75);
+setInterval(updateBackground, 1000 / 60);
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+       // left arrow
+       left -= 5;
+       setState('WALK');
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+       left += 5;
+       setState('WALK');
+    }
+}
+
+function updateBackground() {
+    currentBackground.style.objectPosition = `${-left}px`;
+}
 
 function moveCharacter(){
     pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
@@ -14,6 +37,8 @@ function moveCharacter(){
 
 
 function setState(newState) {
-    frame = 0;
-    state = newState;
+    if(state !== newState) {
+        frame = 0;
+        state = newState;
+    }  
 }
