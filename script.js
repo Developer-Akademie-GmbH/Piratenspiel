ce558e36b43b3855999ff7be6cd40bd1d3f3757b
Junus Ergin <JunusErgin@users.noreply.github.com>
2024-09-17 14:24:50 +0200

    Several Animations work now

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let frame = 0;
+let state = 'IDLE';
 
+setInterval(moveCharacter,75);
 
 function moveCharacter(){
-    pirate.src = `img/2/2_entity_000_IDLE_00${frame}.png`;
+    pirate.src = `img/2/2_entity_000_${state}_00${frame}.png`;
     frame++;
 
     if(frame == 7) {
         frame = 0;
     }
+}
+
+
+function setState(newState) {
+    frame = 0;
+    state = newState;
 }
